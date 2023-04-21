b09b986497889735498a62d65b3c669c414aae11
Marc Delagrammatikas <marc@mechanical-orchard.com>
2023-04-21 14:55:28 -0700

    version 0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OpenTelemetryLogExporter.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @github_page "https://github.com/marcdel/opentelemetry_log_exporter"
 
   def project do
